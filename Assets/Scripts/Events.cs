e05f3714a11b85e9a2899abbea76bdd144b401f5
Gabriel Coffland <35353420+GCoffland@users.noreply.github.com>
2020-03-07 13:51:41 -0800

    be afraid 2?

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public static Events instance;
+    public Dictionary<System.Type, Event> events;
+    public Event owlEvent;
+    public Event owlBouncedOffBuilding;
+
+    private bool owlSpooked;
+
+    // On this line at 1:24 pm, gabe was wrong
+    private void Awake()
+    {
+        instance = this;
+        events = new Dictionary<System.Type, Event>();
+    }
+
+    public static void addEvent(System.Type type, Event e)
+    {
+        instance.events.Add(type, e);
+    }
+
+    public static Event getEventByType(System.Type type)
+    {
+        return instance.events[type];
+    }
+
+
+}
