e2be8fc7148e6c091f2c40278b0524611f2b136c
Jacob Coffland <jacobcoffland@yahoo.com>
2020-03-08 13:16:23 -0700

    merging

--- Assets/Scripts/Events/OwlEvent.cs
+++ Assets/Scripts/Events/OwlEvent.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OwlEvent : Event
 {
     public GameObject owl;
+    public GameObject exclamation;
 
     // Update is called once per frame
     void Update()
@@ -16,16 +17,22 @@ public class OwlEvent : Event
     {
         if (occured)
             return;
-        Debug.Log("the owl event occured!");
-        if (owl != null)
-        {
-            if (Events.getEventByType(typeof(CardboardBoxInteractEvent)).occured)
-                owl.GetComponent<Animator>().Play("OwlSwoopAtRats");
-            else
-                owl.GetComponent<Animator>().Play("OwlSwoopIntoBuilding");
-        }
-        SoundEffectPlayer.instance.Play("Sounds/SoundEffects/OwlTakeOff");
+        StartCoroutine(occurHelper());
         base.occur();
+    }
+
+    public IEnumerator occurHelper()
+    {
+
+        Debug.Log("the owl event occured!");
+        yield return new WaitForSeconds(.5f);
+        exclamation.GetComponent<Animator>().Play("ShowExclamation");
+        yield return new WaitForSeconds(1.25f);
+        if (Events.getEventByType(typeof(CardboardBoxInteractEvent)).occured)
+            owl.GetComponent<Animator>().Play("OwlSwoopAtRats");
+        else
+            owl.GetComponent<Animator>().Play("OwlSwoopIntoBuilding");
+        SoundEffectPlayer.instance.Play("Sounds/SoundEffects/OwlTakeOff");
     }
 
 }
