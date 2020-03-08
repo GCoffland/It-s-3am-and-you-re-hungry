6e36626a514c874758766c9b8406365c0ce9107c
Jacob Coffland <jacobcoffland@yahoo.com>
2020-03-08 01:23:15 -0800

    f

--- Assets/ScaleParallaxBehaviour.cs
+++ Assets/ScaleParallaxBehaviour.cs
@@ -8,6 +8,7 @@ public class ScaleParallaxBehaviour : MonoBehaviour
     GameObject player;
     public CameraController cam;
     public float amount = 1;
+    public bool reverse = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,10 @@ public class ScaleParallaxBehaviour : MonoBehaviour
     void Update()
     {
         Vector3 newScale;
-        newScale = scale * .98f + new Vector3(player.transform.position.x * amount / cam.width, 0, 0);
+        if(reverse)
+            newScale = scale - new Vector3((player.transform.position.x + 0.2f ) * amount / cam.width, 0, 0);
+        else
+            newScale = scale * .98f + new Vector3(player.transform.position.x * amount / cam.width, 0, 0);
         transform.localScale = newScale;
     }
 }
